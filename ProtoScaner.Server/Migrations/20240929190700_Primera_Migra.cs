49aed2d90b7917b71c2c62cbb5299d2353b8fe4a
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-09-29 16:47:08 -0400

    GC - Migraciones

---
+++ ProtoScaner.Server/Migrations/20240929190700_Primera_Migra.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProtoScaner.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primera_Migra : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autors", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autors");
+        }
+    }
+}
