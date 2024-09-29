92fa83d009843af213c270b28845ee7f2bf1713d
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-09-29 16:48:24 -0400

    GC - He agregado la clase Usuarios

---
+++ ProtoScaner.Server/Models/Usuarios.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProtoScaner.Server.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Nombre { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string? PasswordHash { get; set; }
+
+        public int? IdRol { get; set; }
+
+        public DateTime? FechaCreacion { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+    }
+}
