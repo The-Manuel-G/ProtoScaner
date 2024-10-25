d886ee8da8c31cfc5b2505d4a4cc89a2ac189a4e
The-Manuel-G <arielnaranjoi829@gmail.com>
2024-10-25 11:51:09 -0400

    .

--- protoscaner.client/src/App.tsx
+++ protoscaner.client/src/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
 import React from 'react';
-import NavigationMenu from './NavigationMenu'; // Importa el NavigationMenu desde la carpeta components
+// Importa el NavigationMenu desde la carpeta components
 
 const App: React.FC = () => {
     return (
         <div>
-            <NavigationMenu />
+         
             <div className="text-center mt-4">
                 <h1 className="text-2xl font-bold">Bienvenido a Mi App</h1>
                 <p>Aquí puedes navegar por las diferentes secciones.</p>
